36d0a68a6ce494da752984f4683ae10da50bb715
Sam Fields <samuelrfields@naranja.io>
2016-10-18 00:33:48 -0500

    Created a wifi definition template.

---
+++ WifiDefsTemplate.h
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+// WifiDefsTemplate.h - Definitions specific to the network the esp8266 will be
+// connecting to. This file is a template. Change the values to your own wifi
+// network and rename this file to WifiDefs.h.
+//
+// Copyright 2016 Samuel R Fields
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef WIFI_DEFS_H
+#define WIFI_DEFS_H
+
+const char* ssid     = "network";
+const char* password = "password";
+
+#endif
